d5fba7ea3e0f027c9b50caabf383182d67a6793d
Godeffroy Valet <godeffroy@swave.io>
2025-08-20 14:18:40 +0200

    Replace test_code.py by CLI program_dlpc900.py

---
+++ program_dlpc900.py
@@ -0,0 +1,37 @@
+import argparse
+parser = argparse.ArgumentParser(description="Programs a sequence of images through Texas Instruments' DLPLCRC900EVM evaluation module for DLP displays")
+parser.add_argument("-i", "--images", nargs='+', help="Input images", required=True)
+parser.add_argument("--fps", type=int, default=3)
+parser.add_argument("--sum", action='store_true')
+args = parser.parse_args()
+
+
+import pycrafter6500
+import numpy
+from PIL import Image, ImageChops, ImageOps
+
+Image.MAX_IMAGE_PIXELS = None
+
+def as_binary_np_array(image):
+    return numpy.asarray(image.convert('L'))//129
+
+dlp_resolution=(1920,1080)
+
+# Load input images
+images=[as_binary_np_array(ImageOps.pad(Image.open(path), size=dlp_resolution, color='black', centering=(0.5, 0.5))) for path in args.images]
+if args.sum:
+    images=[sum(images)]+[0*images[0] for _ in range(len(images)-1)]
+
+# Program the display controller
+dlp=pycrafter6500.dmd()
+dlp.stopsequence()
+dlp.changemode(3)
+
+exposure=[1000000//args.fps]*30
+dark_time=[0]*30
+trigger_in=[False]*30
+trigger_out=[1]*30
+
+dlp.defsequence(images,exposure,trigger_in,dark_time,trigger_out,0)
+
+dlp.startsequence()
